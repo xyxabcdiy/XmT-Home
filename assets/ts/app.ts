9797057dbfb26987958e896ec5a966ea8ebc698e
xyxabcdiy <xyxabcdiy@gmail.com>
2015-09-23 17:57:27 +0800

    Added method getResentArticles in class App

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -15,6 +15,36 @@ class App {
     public init() {
         this.handleBootstrap();
         this.handleHeader();
+        this.getResentArticles();
+    }
+
+    public getResentArticles() {
+        let self = this;
+        $.ajax({
+            type: "GET",
+            url: "/webapi/xmter/resent",
+            dataType: "json",
+            beforeSend: function () {
+                $(".latest-list").before('<div class="spinner-next" id="xmterResentLoading"><div class="rect1"></div> <div class="rect2"></div> <div class="rect3"></div> <div class="rect4"></div> <div class="rect5"></div> </div>');
+            },
+            complete: function () {
+                $("#xmterResentLoading").remove();
+            },
+            success: function (data) {
+                if (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        var href:string = "/blog/details?articleId=" + data[i].id;
+                        $(".latest-list").append('<li><a href="' + href + '">' + data[i].title + '</a><small>' +
+                            self.publicationTime(Number(data[i].date)) + '</small></li>');
+                    }
+                } else {
+                    $(".latest-list").empty().append('<li><p>对不起，暂无更新~！<p></li>');
+                }
+            },
+            error: function () {
+                $(".latest-list").empty().append('<li><p>对不起，暂无更新~！<p></li>');
+            }
+        });
     }
 
     public initParallaxBg() {
@@ -41,6 +71,20 @@ class App {
             interval: 2500,
             pause: "hover"
         });
+    }
+
+    public publicationTime(date:number) {
+        let dateObj = new Date(date);
+        let interval = (Date.now() - dateObj.getTime()) / 1000 / 60 / 60;
+        if (interval > 24) {
+            let htmlDate = ' ';
+            htmlDate += dateObj.getFullYear() + ' . ' + (dateObj.getMonth() + 1) + ' . ' + dateObj.getDate();
+            return htmlDate;
+        } else if (interval > 1) {
+            return Math.ceil(interval) + " 小时之前";
+        } else {
+            return Math.ceil(interval * 60) + " 分钟之前";
+        }
     }
 
     static alert(message){
